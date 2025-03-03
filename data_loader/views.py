38b9a81acf98d75619fdffff9fb56940c2129b0f
jasondza2010 <jasondza2010@gmail.com>
2025-03-03 23:01:26 +0530

    Initial commits

---
+++ data_loader/views.py
@@ -0,0 +1,78 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from ecomm_dashboard.visualizer.models import Platform
+import pandas as pd
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+@api_view(["POST"])
+def extract_order_data(request):
+    """
+    API endpoint that returns sales volume based on optional filters.
+    """
+    try:
+        platform_objs = Platform.objects.filter(name="Flipkart")
+
+        print(
+            "platforms_filteredplatforms_filtered",
+            [platform.name for platform in platform_objs],
+        )
+
+        # Retrieve optional filters from query parameters
+        filters = {
+            "sales_date": request.query_params.get("date_range", None),
+            "product_category": request.query_params.get("product_category", None),
+            "delivery_status": request.query_params.get("delivery_status", None),
+            "platform": request.query_params.get("platform", None),
+            "state": request.query_params.get("state", None),
+        }
+
+        # Start with the full DataFrame
+        filtered_data = pd.DataFrame(
+            [],
+            columns=[
+                "sales_date",
+                "product_category",
+                "delivery_status",
+                "platform",
+                "state",
+            ],
+        )
+
+        # Apply filters if provided
+        if filters["sales_date"]:
+            try:
+                start_date, end_date = filters["sales_date"].split(",")
+                filtered_data = filtered_data[
+                    (filtered_data["sales_date"] >= start_date)
+                    & (filtered_data["sales_date"] <= end_date)
+                ]
+            except ValueError:
+                return Response(
+                    {
+                        "status": "error",
+                        "message": "Invalid date range format. Use 'start_date,end_date'.",
+                    },
+                    status=400,
+                )
+
+        for key in ["product_category", "delivery_status", "platform", "state"]:
+            if filters[key]:
+                filtered_data = filtered_data[filtered_data[key] == filters[key]]
+
+        # Return the filtered results
+        return Response(
+            {"status": "success", "data": filtered_data.to_dict(orient="records")}
+        )
+    except Exception as e:
+        logger.error(f"Error processing request: {str(e)}")
+        return Response(
+            {
+                "status": "error",
+                "message": "An error occurred while processing your request.",
+            },
+            status=500,
+        )
---
+++ visualizer/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Platform(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        app_label = "visualizer"
+        db_table = "platform"
+
+    def __str__(self):
+        return self.id
+
+
+class Product(models.Model):
+    id = models.CharField(primary_key=True)
+    name = models.CharField(max_length=32)
+    category = models.CharField(max_length=32)
+    platform_id = models.ForeignKey(Platform, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = "visualizer"
+        db_table = "product"
+
+    def __str__(self):
+        return self.id
+
+
+class Customer(models.Model):
+    id = models.CharField(primary_key=True)
+    name = models.CharField(max_length=32)
+    contact_email = models.CharField(max_length=255)
+    platform_id = models.ForeignKey(Platform, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = "visualizer"
+        db_table = "customer"
+
+    def __str__(self):
+        return self.id
+
+
+class CustomerAddressDetails(models.Model):
+    id = models.CharField(primary_key=True)
+    street = models.CharField(max_length=32)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    pincode = models.IntegerField()
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = "visualizer"
+        db_table = "customer_address_details"
+
+    def __str__(self):
+        return self.id
+
+
+class Order(models.Model):
+    id = models.CharField(primary_key=True)
+    date_of_sale = models.DateField()
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer_address_details_id = models.ForeignKey(
+        CustomerAddressDetails, on_delete=models.CASCADE
+    )
+    platform_id = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    delivery_date = models.DateField()
+    delivery_status = models.CharField(max_length=31)
+    meta_data = models.JSONField()
+
+    class Meta:
+        app_label = "visualizer"
+        db_table = "order"
+
+    def __str__(self):
+        return self.id
+
+
+class OrderDetails(models.Model):
+    id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    selling_price = models.FloatField()
+
+    class Meta:
+        app_label = "visualizer"
+        db_table = "order_details"
+
+    def __str__(self):
+        return self.id
